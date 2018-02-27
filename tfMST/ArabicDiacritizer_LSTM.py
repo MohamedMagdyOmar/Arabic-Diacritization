327fdc58d35f98a5fd55c0de4a28d1b7c61a7b13
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2018-02-27 11:42:04 +0200

    add more visibility messages

--- tfMST/ArabicDiacritizer_LSTM.py
+++ tfMST/ArabicDiacritizer_LSTM.py
@@ -29,9 +29,19 @@ def get_training_data():
     return x, y
 
 
-def create_vocab():
+def get_testing_data():
     DBHelperMethod.connect_to_db()
+    training_dataset = DBHelperMethod.load_dataset_by_type("testing")
 
+    x = dp.load_nn_input_dataset_string(training_dataset[:, [0, 6]])
+    y = dp.load_nn_labels_dataset_string(training_dataset[:, [0, 1]])
+
+    return x, y
+
+
+def create_vocab():
+
+    DBHelperMethod.connect_to_db()
     dataset = DBHelperMethod.load_data_set()
     chars = dp.load_nn_input_dataset_string(dataset[:, [0, 6]])
     vocab, vocab_inv = dp.build_vocab(chars)
@@ -43,46 +53,62 @@ def get_chars_and_vocab_count(vocab, chars):
 
 
 if __name__ == "__main__":
-    X_train, Y_train = get_training_data()
+    dataX_train, dataY_train = get_training_data()
+    dataX_test, dataY_test = get_testing_data()
+
     vocabulary, vocab_inverse, all_chars, dataset = create_vocab()
     n_chars, n_vocab = get_chars_and_vocab_count(vocabulary, all_chars)
 
     seq_length = 3
-    dataX = []
-    dataY = []
+    X_train = []
+    X_test = []
 
-    for i in range(0, len(X_train) - seq_length, 1):
-        seq_in = X_train[i:i + seq_length]
-        dataX.append([vocabulary[char] for char in seq_in])
+    for i in range(0, len(dataX_train) - seq_length, 1):
+        seq_in = dataX_train[i:i + seq_length]
+        X_train.append([vocabulary[char] for char in seq_in])
 
-    Y = Y_train[0: len(Y_train) - 3]
+    for i in range(0, len(dataX_test) - seq_length, 1):
+        seq_in = dataX_test[i:i + seq_length]
+        X_test.append([vocabulary[char] for char in seq_in])
 
-    n_patterns = len(dataX)
-    print("Total Patterns: ", n_patterns)
+    Y_train = dataY_train[0: len(dataY_train) - 3]
+    Y_test = dataY_test[0: len(dataY_test) - 3]
+
+    n_patterns_train = len(X_train)
+    n_patterns_test = len(X_test)
+
     # reshape X to be [samples, time steps, features]
-    X = numpy.reshape(numpy.array(dataX), (n_patterns, seq_length, 1))
-    # normalize
-    X = X / float(n_vocab)
-    # one hot encode the output variable
+    X_train = numpy.reshape(numpy.array(X_train), (n_patterns_train, seq_length, 1))
+    X_test = numpy.reshape(numpy.array(X_test), (n_patterns_test, seq_length, 1))
 
-    print(X.shape[1])
+    # normalize
+    X_train = X_train / float(n_vocab)
+    X_test = X_test / float(n_vocab)
 
     model = Sequential()
-    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True, name="LSTM1"))
+    model.add(LSTM(256, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True, name="LSTM1"))
 
     model.add(Dropout(0.2))
     model.add(LSTM(256, name="LSTM2"))
     model.add(Dropout(0.2))
-    model.add(Dense(Y.shape[1], activation='softmax', name="dense"))
+    model.add(Dense(Y_test.shape[1], activation='softmax', name="dense"))
     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    # define the checkpoint
-    file_path = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
-    checkpoint = ModelCheckpoint(file_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    callbacks_list = [checkpoint]
+    # file_path = "weights.best.hdf5"
+    checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1,
+                                 save_best_only=True, mode='max')
+
+    # check 5 epochs
+    early_stop = EarlyStopping(monitor='val_acc', patience=10, mode='max')
+    callbacks_list = [checkpoint, early_stop]
 
     # fit the model
     model.summary()
-    model.fit(X, Y, epochs=50, batch_size=64, callbacks=callbacks_list)
+    model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
+              callbacks=callbacks_list, epochs=50, batch_size=64, verbose=1)
+
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, Y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1] * 100))
 
 
